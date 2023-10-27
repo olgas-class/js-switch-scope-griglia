b4f474d540160527f9e909962d172f37671767dd
olga_demina <olga.demina1410@gmail.com>
2023-10-27 11:15:08 +0200

    add: switch example

---
+++ switch/js/script.js
@@ -0,0 +1,20 @@
+const userFruit = "mela";
+
+// if(userFruit === "mela") {
+//     console.log("Mi piace la mela");
+// } else if(userFruit === "pera") {
+//     console.log("Anche pera mi piace");
+// } else {
+//     console.log("Non ho provato questo frutto");
+// }
+
+switch(userFruit) {
+    case "mela":
+        console.log("Mi piace la mela");
+        break;  
+    case "pera":
+        console.log("Anche pera mi piace");
+        break;
+    default: 
+        console.log("Non ho provato questo frutto");
+}
